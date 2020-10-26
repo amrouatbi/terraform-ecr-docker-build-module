c547fdb6afdf3215c04622a629cf978d6e097901
Nicolas Douchin <nicolas.douchin@sia-partners.com>
2020-10-26 15:18:35 +0100

    No more need of region as aws cli
    Signed-off-by: Nicolas Douchin <nicolas.douchin@sia-partners.com>

--- bin/build.sh
+++ bin/build.sh
@@ -6,15 +6,6 @@ set -e
 # This is the order of arguments
 build_folder=$1
 aws_ecr_repository_url_with_tag=$2
-# kept for backwards compatibility
-aws_region=$3
-
-# Allow overriding the aws region from system
-if [ "$aws_region" != "" ]; then
-  aws_extra_flags="--region $aws_region"
-else
-  aws_extra_flags=""
-fi
 
 # Check that docker is installed and running
 which docker > /dev/null && docker ps > /dev/null || { echo 'ERROR: docker is not running' ; exit 1; }
@@ -28,7 +19,8 @@ docker run \
     ecr get-login-password \
     | docker login \
     --username AWS \
-    --password-stdin $aws_ecr_repository_url_with_tag
+    --password-stdin \
+    $aws_ecr_repository_url_with_tag
 
 # Some Useful Debug
 echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
