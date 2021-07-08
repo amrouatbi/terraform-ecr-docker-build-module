7d7c06257dae3dbd6ac8f773c797439c2448422a
amrouatbi <52835574+amrouatbi@users.noreply.github.com>
2021-07-08 10:44:54 +0200

    Update build.sh
    Add build-arg to docker build

--- bin/build.sh
+++ bin/build.sh
@@ -6,6 +6,7 @@ set -e
 # This is the order of arguments
 build_folder=$1
 aws_ecr_repository_url_with_tag=$2
+build_arg=$3
 
 # Check that docker is installed and running
 which docker > /dev/null && docker ps > /dev/null || { echo 'ERROR: docker is not running' ; exit 1; }
@@ -29,7 +30,7 @@ echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
 cd $build_folder
 
 # Build image
-docker build -t $aws_ecr_repository_url_with_tag -f Dockerfile ..
+docker build -t $aws_ecr_repository_url_with_tag $build-arg=$3 $build_folder
 
 # Push image
 docker push $aws_ecr_repository_url_with_tag
